d243d008a92e03484b8a36fc5540c26104d69351
Omri Litov <omrilitov@gmail.com>
2015-04-13 21:41:40 +0300

    Opening shell through npm link

--- client/app/app.js
+++ client/app/app.js
@@ -2,5 +2,5 @@
 
 angular.module('nachosApp', ['ngMaterial'])
   .run(function (files) {
-    files.open("E:\\a\\_Afrojack_-_Rock_The_House_Original_Mix_.mp3");
+    //files.open("E:\\a\\_Afrojack_-_Rock_The_House_Original_Mix_.mp3");
   });
--- client/components/shell/index.js
+++ client/components/shell/index.js
@@ -11,9 +11,16 @@ angular.module('nachosApp')
     configuration.configs.get(function (err, file) {
       if (err) return console.log(err);
 
-      var pathToShell = path.resolve('node_modules/nachos-shell/client');
+      var pathToShell = path.resolve('./../node_modules/nachos-shell/client');
       var commandToRun = file.shell || 'nw ' + pathToShell;
       console.log('Starting shell: ' + commandToRun);
+
+      //var json = require('./../node_modules/nachos-shell/client/package.json');
+      //gui.Window.open('./../node_modules/nachos-shell/client/index.html', json.window);
+
+      exec(commandToRun, function (err) {
+        if (err) return console.log(err);
+      });
 
       _.forEach(screen.getAllScreens(), function (screen) {
         /*exec(commandToRun + ' ' + screen.handle, function (err) {
