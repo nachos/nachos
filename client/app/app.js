735cae50d2a3afed8c4d391119274171ee20b182
Elad Bezalel <elad.bezalel@gmail.com>
2015-05-02 17:15:06 +0300

    opening devtools through keybindings

--- client/app/app.js
+++ client/app/app.js
@@ -3,6 +3,16 @@
 angular.module('nachosApp', ['ngMaterial'])
   .run(function (fs, server) {
     //fs.open("E:\\a\\_Afrojack_-_Rock_The_House_Original_Mix_.mp3");
+    var gui = require('nw.gui');
+    var keybindings = require('keybindings');
+    var window = gui.Window.get();
+
+    new keybindings({
+      key: 'f12',
+      keydown: function () {
+        window.showDevTools();
+      }
+    });
 
     server.start();
   });
