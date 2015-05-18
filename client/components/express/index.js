7d947f2a644e830a5b3b4ef93a859e40d69db9d0
Amir Schlezinger <amir.shrir@gmail.com>
2015-05-18 23:23:33 +0300

    When loading the server, randomize the port, retry if taken, and write the successful one in the config

--- client/components/express/index.js
+++ client/components/express/index.js
@@ -2,26 +2,46 @@
 
 angular.module('nachosApp')
   .service('server', function(expressConfig, socketioConfig, serverRoutes) {
+    var self = this;
     var express = require('express');
-
+    var _ = require('lodash');
     var app = express();
     var server = require('http').createServer(app);
     var socketio = require('socket.io')(server, {
       //serveClient: (config.env === 'production') ? false : true,
       //path: '/socket.io-client'
     });
+    var configuration = require('nachos-configuration');
 
     expressConfig(app);
     serverRoutes(app);
     socketioConfig(socketio);
 
+    server.on('error', function (e) {
+      if (e.code == 'EADDRINUSE') {
+        console.log('Address in use, retrying...');
+        self.stop();
+        self.start();
+      }
+    });
+
     this.start = function () {
-      server.listen(9000, 'localhost', function () {
-        console.log('Express server listening on %d', 9000);
+      var port = _.random(1024, 65535);
+      server.listen(port, 'localhost', function () {
+        console.log('Express server listening on %d', port);
+        configuration.configs.get('nachos', function(err, config){
+          if(err)
+            console.log(err);
+          config.port = port;
+          configuration.configs.save('nachos', config, function(err){
+            if(err)
+              console.log(err);
+          })
+        })
       });
     };
 
     this.stop = function () {
-      server.stop();
+      server.close();
     }
   });
